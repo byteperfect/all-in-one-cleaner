7829d50dcb16903846c64c102111ee1e52655407
Aleksandr Levashov <aleksandr@byteperfect.dev>
2024-02-14 20:19:03 +0300

    Added license.

--- all-in-one-cleaner.php
+++ all-in-one-cleaner.php
@@ -10,8 +10,24 @@
  * Requires PHP:      7.4
  * Text Domain:       all-in-one-cleaner
  * Domain Path:       /languages/
+ * License:           GPLv3
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
  *
  * @package all_in_one_cleaner
+ */
+
+/**
+ * "All-In-One Cleaner" is free software:
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * version 3.
+ *
+ * "All-In-One Cleaner" is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with "All-In-One Cleaner".
+ * If not, see https://www.gnu.org/licenses/gpl-3.0.html.
  */
 
 declare( strict_types=1 );
